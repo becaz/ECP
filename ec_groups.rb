b9b87a12ca2258ec03db6596c366ce03985df33c
Bayram <bayram@becaz.org>
2020-02-14 14:07:25 +0400

    Add a new example

--- ec_groups.rb
+++ ec_groups.rb
@@ -62,4 +62,17 @@ def example3
   19.times{|i| puts i*gen}
 end
 
-example3
+def example4
+  gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+  gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+  p = 2**256 - 2**32 - 977
+  n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+  x = FieldElement.new(value: gx, prime: p)
+  y = FieldElement.new(value: gy, prime: p)
+  seven = FieldElement.new(value: 7, prime: p)
+  zero = FieldElement.new(value: 0, prime: p)
+  g = ECPoint.new(a: zero, b: seven, x: x, y: y)
+  puts n*g # => Point(identity)
+end
+
+example4
